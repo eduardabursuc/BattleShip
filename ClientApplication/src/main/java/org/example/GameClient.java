7afa2691002b3169d2b2685dcb0233530ebe8b23
vanillaxqz <vanillaxyz@protonmail.com>
2024-06-01 13:09:45 +0300

    nearly done gui

--- ClientApplication/src/main/java/org/example/GameClient.java
+++ ClientApplication/src/main/java/org/example/GameClient.java
@@ -1,9 +1,12 @@
 package org.example;
 
-import java.io.*;
-import java.net.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class GameClient {
     private String serverAddress;
@@ -12,6 +15,7 @@ public class GameClient {
     private AtomicBoolean running = new AtomicBoolean(true);
     private PrintWriter writer;
     GameClientGUI gui;
+
     public GameClient(String serverAddress, int serverPort) {
         this.serverAddress = serverAddress;
         this.serverPort = serverPort;
@@ -59,6 +63,14 @@ public class GameClient {
         }
     }
 
+    public void sendSetShipMessage(String from, String to) {
+        sendMessage("set ship " + from + " " + to);
+    }
+
+    public void sendSubmitMoveMessage(String move) {
+        sendMessage("submit move " + move);
+    }
+
     private class ServerResponseHandler implements Runnable {
         private Socket socket;
         private AtomicBoolean running;
@@ -79,13 +91,90 @@ public class GameClient {
                     }
                     if (response.startsWith("Game created with ID:")) {
                         String finalResponse = response;
-                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, finalResponse, "Game Created", JOptionPane.INFORMATION_MESSAGE));
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, finalResponse + "\n Waiting for the other player to join.", "Game Created", JOptionPane.INFORMATION_MESSAGE));
                         gui.showGameCreatedPopup(response.split(":")[1].trim());
                     }
                     if (response.startsWith("Joined game with ID:")) {
                         String finalResponse = response;
                         SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, finalResponse, "Game Joined", JOptionPane.INFORMATION_MESSAGE));
                         gui.showGameCreatedPopup(response.split(":")[1].trim());
+                    }
+                    if (response.startsWith("Create")) {
+                        SwingUtilities.invokeLater(() -> gui.createSouthPanel(gui.southPanel, true));
+                    }
+                    if (response.startsWith("Your board is created")) {
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "Board is set.", "Board is set.", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if (response.startsWith("Ship placed successfully")) {
+                        String from = gui.getStartTextField().getText();
+                        String to = gui.getEndTextField().getText();
+                        gui.updateMatrixWithShip(from, to);
+                        String finalResponse = response;
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, finalResponse, "Ship Placed", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if (response.startsWith("Game started")) {
+                        gui.resetBoard();
+                        SwingUtilities.invokeLater(() -> gui.createSouthPanel(gui.southPanel, false));
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "Game started!", "Game Started", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if(response.startsWith("Your turn")) {
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "It's your turn!", "Your Turn", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if(response.startsWith("The game is over.")) {
+                        SwingUtilities.invokeLater(() -> {
+                            JFrame frame = new JFrame();
+                            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            int result = JOptionPane.showOptionDialog(
+                                    frame,
+                                    "You lost!",
+                                    "LOSE",
+                                    JOptionPane.DEFAULT_OPTION,
+                                    JOptionPane.INFORMATION_MESSAGE,
+                                    null,
+                                    new Object[]{"OK"},
+                                    "OK"
+                            );
+                            if (result == JOptionPane.OK_OPTION) {
+                                System.exit(0);
+                            }
+                        });
+                    }
+                    if(response.startsWith("It's not your turn.")) {
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "It's not your turn.", "Error", JOptionPane.ERROR_MESSAGE));
+                    }
+                    if(response.startsWith("You attacked a ship")) {
+                        String cell = gui.getAttackTextField().getText();
+                        gui.updateMatrixWithAttack(cell, true);
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "You attacked a ship!", "HIT", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if(response.startsWith("Oops ... missed")) {
+                        String cell = gui.getAttackTextField().getText();
+                        gui.updateMatrixWithAttack(cell, false);
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "Oops ... missed the target.", "MISS", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if(response.startsWith("The ship sank")) {
+                        String cell = gui.getAttackTextField().getText();
+                        gui.updateMatrixWithAttack(cell, true);
+                        SwingUtilities.invokeLater(() -> JOptionPane.showMessageDialog(null, "The ship sank!", "SUNK", JOptionPane.INFORMATION_MESSAGE));
+                    }
+                    if(response.startsWith("Congratulations")) {
+                        SwingUtilities.invokeLater(() -> {
+                            JFrame frame = new JFrame();
+                            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                            int result = JOptionPane.showOptionDialog(
+                                    frame,
+                                    "You won!",
+                                    "Game Over",
+                                    JOptionPane.DEFAULT_OPTION,
+                                    JOptionPane.INFORMATION_MESSAGE,
+                                    null,
+                                    new Object[]{"OK"},
+                                    "OK"
+                            );
+                            if (result == JOptionPane.OK_OPTION) {
+                                System.exit(0);
+                            }
+                        });
                     }
                 }
             } catch (IOException e) {
@@ -97,8 +186,8 @@ public class GameClient {
     }
 
     public static void main(String[] args) {
-        String serverAddress = "localhost"; // Change this to your server address
-        int serverPort = 9999; // Change this to your server port number
+        String serverAddress = "localhost";
+        int serverPort = 9999;
         GameClient client = new GameClient(serverAddress, serverPort);
         client.startClient();
     }
